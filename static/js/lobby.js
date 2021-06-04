956b26eae2b643cf5696d14a4cb8c6acfeefe9aa
Jakub <kubatom3@gmail.com>
2021-06-04 15:04:19 +0200

    separated javascript from templates

---
+++ static/js/lobby.js
@@ -0,0 +1,36 @@
+const roomName = JSON.parse(document.getElementById('room-name').textContent);
+
+    const lobbySocket = new WebSocket(
+        'ws://'
+        + window.location.host
+        + '/ws/lobby/'
+        + roomName
+        + '/'
+    );
+
+    lobbySocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        if (data['message'] === 'exit') {
+            document.querySelector('#number_of_players').innerHTML = 'Twórca pokoju wyszedł z gry. Stwórz własną lub dołącz do istniejącej';
+        } else {
+            document.querySelector('#number_of_players').innerHTML = data['message'];
+        }
+    };
+
+    lobbySocket.onclose = function(e) {
+        console.error('Chat socket closed unexpectedly');
+    };
+
+    document.querySelector('#start_game').onkeyup = function(e) {
+        if (e.keyCode === 13) {
+                document.querySelector('#start_game').click();
+        }
+    };
+
+    document.querySelector('#start_game').onclick = function(e) {
+        lobbySocket.send(JSON.stringify({
+            'message': {
+                'event': 'start',
+            }
+        }));
+    };
